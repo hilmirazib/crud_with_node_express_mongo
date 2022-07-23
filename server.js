9d3c0fa85dcb5ca48ba641b0e8580bd9df35049a
hilmi.inolabs <hilmi.inolabs@gmail.com>
2022-07-23 14:10:45 +0700

    latihan crud nodejs, expressjs, mongodb

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const connectDB = require('./server/database/connection');
+const app = express();
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080;
+//log requests
+app.use(morgan('tiny'));
+
+//mongodb connection
+connectDB();
+
+//parse requests tp body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//set views engine
+app.set('view engine', 'ejs');
+// app.set('views', path.resolve(__dirname, 'views/ejs'));
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
+
+//load routes
+app.use('/', require('./server/routes/router'));
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
